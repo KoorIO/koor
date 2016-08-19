bcc474f259ea2c38257522cc0d05f74ea32f6875
thanhson1085 <thanhson1085@gmail.com>
2016-08-19 06:41:14 +0000

    dynamic header :water_buffalo:

--- site/app/scripts/controllers/apis.js
+++ site/app/scripts/controllers/apis.js
@@ -12,17 +12,35 @@ angular.module('siteSeedApp')
     af.contentType = 'application/json';
     af.contentEncoding = 'UTF-8';
     af.responseBody = JSON.stringify({});
+
+    af.headers = [];
+    af.addHeader = function() {
+        af.headers.push({
+            key: '',
+            value: ''
+        });
+    };
+    af.removeHeader = function(k) {
+        af.headers.splice(k,1);
+    };
+
     af.create = function() {
+
+        var headers = {
+            'Content-Type': af.contentType,
+            'Content-Encoding': af.contentEncoding
+        };
+        af.headers.forEach(function(item) {
+            headers[item.key] = item.value;
+        });
+
         var data  = {
             path: af.path,
             projectId: projectId,
             name: af.method + ' ' + af.path,
             method: af.method,
             response: {
-                headers: {
-                    contentType: af.contentType,
-                    contentEncoding: af.contentEncoding
-                },
+                headers: headers,
                 status: af.responseStatus,
                 body: JSON.parse(af.responseBody)
             }
@@ -36,29 +54,54 @@ angular.module('siteSeedApp')
     var af = this;
     var projectId = $stateParams.projectId;
     var apiId = $stateParams.apiId;
+
+    af.headers = [];
+    af.addHeader = function() {
+        af.headers.push({
+            key: '',
+            value: ''
+        });
+    };
+    af.removeHeader = function(k) {
+        af.headers.splice(k,1);
+    };
+
     Projects.get($stateParams.projectId).then(function(response) {
         $scope.project = response;
         Apis.get(apiId).then(function(res) {
             af.method = res.method;
             af.path = res.path;
             af.responseStatus = res.response.status;
-            af.contentType = res.response.headers.contentType;
-            af.contentEncoding = res.response.headers.contentEncoding;
-            af.responseBody = JSON.stringify(res.response.body, null, '\t');
+            af.contentType = res.response.headers['Content-Type'];
+            af.contentEncoding = res.response.headers['Content-Encoding'];
+            var responseBody = res.response.body || {};
+            af.responseBody = JSON.stringify(responseBody, null, '\t');
+            Object.keys(res.response.headers).forEach(function(key) {
+                if (key !== 'Content-Type' && key !== 'Content-Encoding') {
+                    af.headers.push({
+                        key: key,
+                        value: res.response.headers[key]
+                    });
+                }
+            });
         });
     });
 
     af.update = function() {
+        var headers = {
+            'Content-Type': af.contentType,
+            'Content-Encoding': af.contentEncoding
+        };
+        af.headers.forEach(function(item) {
+            headers[item.key] = item.value;
+        });
         var data  = {
             path: af.path,
             projectId: projectId,
             name: af.method + ' ' + af.path,
             method: af.method,
             response: {
-                headers: {
-                    contentType: af.contentType,
-                    contentEncoding: af.contentEncoding
-                },
+                headers: headers,
                 status: af.responseStatus,
                 body: JSON.parse(af.responseBody)
             }
