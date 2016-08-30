8cb369af2a514b98d50980ea4883faf9bf57a8d5
thanhson1085 <thanhson1085@gmail.com>
2016-08-30 03:48:12 +0000

    add landing page :pig:

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ workdir=${PWD}
 
 cd $workdir/users && nodemon index.js &
 cd $workdir/apps && nodemon index.js &
-cd $workdir/site && grunt serve
+cd $workdir/site && grunt serve &
+cd $workdir/land && gulp dev
