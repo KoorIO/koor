66b96681d61142f173f08d8cad67aaa3376b488f
thanhson1085 <thanhson1085@gmail.com>
2016-09-01 11:00:31 +0000

    hanldle SIGINT :fish:

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+_interupt() { 
+    echo "Shutdown $child_proc"
+    kill -TERM "$child_proc" 2>/dev/null
+    exit
+}
+
+trap _interupt INT TERM
 
 workdir=${PWD} 
 
 cd $workdir/users && nodemon index.js &
+child_proc=$! 
 cd $workdir/apps && nodemon index.js &
+child_proc="$child_proc $!"
 cd $workdir/site && grunt serve &
+child_proc="$child_proc $!"
 cd $workdir/land && gulp dev
+child_proc="$child_proc $!"
