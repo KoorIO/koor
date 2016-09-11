4e05339e3253091ba9ef857aeb8999c43c717f04
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-09-11 08:05:06 +0000

    iot service :smile:

---
+++ iot/index.js
@@ -0,0 +1,14 @@
+var mqtt = require('mqtt');
+var config = require('config');
+var client  = mqtt.connect([config.get('broker.url')]);
+ 
+client.on('connect', function () {
+  client.subscribe('presence');
+  client.publish('presence', 'Hello mqtt');
+});
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(topic, message.toString());
+  client.end();
+});
