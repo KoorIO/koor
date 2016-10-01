296aa90dc3c31386feb6ab1baf1cb1faa3fa6250
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-10-01 09:30:42 +0000

    some small change in UI :snake:

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure(2) do |config|
 
   config.vm.provider "virtualbox" do |vb|
     vb.gui = false
-    vb.memory = 1024
+    vb.memory = 2048
     vb.cpus = 2
   end
 
--- ops/init.sh
+++ ops/init.sh
@@ -10,3 +10,12 @@ wget -qO- https://get.docker.com/ | sh
 echo Installing Docker Compose
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+echo Installing VirtualEnv ...
+apt-get install -y python-virtualenv
+
+echo Installing NodeJS 4x ...
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get install -y nodejs
+npm install -g npm@latest
+npm install -g nodemon grunt-cli gulp
