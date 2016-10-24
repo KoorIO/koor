a62653e096004887f01e2e8dcc3ec51d1aae901d
thanhson1085 <thanhson1085@gmail.com>
2016-10-24 03:30:59 +0000

    remove iot subscriber :bug:

--- iot/index.js
+++ iot/index.js
@@ -5,7 +5,7 @@ var client = mqtt.connect(config.get('broker.url'));
 
 client.on('connect', function (packet) {
     console.log('Connected');
-    client.subscribe('580cdb2e089c0a000d8efc03.koor.io/devices/580cdb3c089c0a000d8efc04');
+    //client.subscribe('580cdb2e089c0a000d8efc03.koor.io/devices/580cdb3c089c0a000d8efc04');
 });
 
 client.on('message', function (topic, message) {
