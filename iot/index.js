e43d06e7179c89a552121f38a83f6ead4f36cdcc
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-10-30 15:09:40 +0000

    prepare for deploy :ant:

--- iot/index.js
+++ iot/index.js
@@ -5,7 +5,7 @@ var client = mqtt.connect(config.get('broker.url'));
 
 client.on('connect', function (packet) {
     console.log('Connected');
-    client.subscribe('5814db3171d02f000eca3cf8.koor.io/devices/5814db41818638001b8e8bfa');
+    //client.subscribe('5814db3171d02f000eca3cf8.koor.io/devices/5814db41818638001b8e8bfa');
 });
 
 client.on('message', function (topic, message) {
