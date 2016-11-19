e2f938f6e612ae948c0725cd81ad310acc842539
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-11-19 16:51:29 +0000

    mutual friends :rose:

--- users/apis/recommendations.js
+++ users/apis/recommendations.js
@@ -1,34 +1,33 @@
 'use strict';
 var express = require('express'), 
     logger = require('../helpers/logger'),
-    driver = require('../helpers/neo4j'),
     es = require('../models/elasticsearch'),
+    nj = require('../models/neo4j'),
     router = express.Router();
 
-// People May You Know
-router.get('/peopleMayYouKnow/:page/:limit', function(req, res){
+// People You May Know
+router.get('/peopleYouMayKnow/:page/:limit', function(req, res){
     var limit = (req.params.limit)? parseInt(req.params.limit): 10;
     var skip = (req.params.page)? limit * (req.params.page - 1): 0;
     logger.debug('Get People May You Know', req.body.userId, limit, skip);
-    var session = driver.session();
-    session
-    .run('MATCH (u:Users)-[:FOLLOW]->(f:Users)-[:FOLLOW]->(p:Users) \
-        WHERE u.userId = {userId} RETURN DISTINCT p \
-        SKIP {skip} \
-        LIMIT {limit} \
-        ', {userId: req.body.userId, limit: limit, skip: skip})
-    .then(function(result) {
-        var userIds = [];
-        result.records.forEach(function(record) {
-            userIds.push(record._fields[0].properties.userId);
-        });
-        // Completed!
-        session.close();
+    nj.User.peopleYouMayKnow({
+        userId: req.body.userId,
+        limit: limit,
+        skip: skip
+    }).then(function(userIds) {
         es.User.searchByUserIds({
             userIds: userIds,
             from: skip,
             size: limit
         }).then(function(response) {
+            for (var i in response['hits']) {
+                nj.User.countMutualFriends({
+                    aId: req.body.userId,
+                    bId: response['hits'][i]._id
+                }).then(function(result) {
+                    console.log(result.records);
+                });
+            }
             return res.json(response);
         }).catch(function(e) {
             return res.json({});
--- users/models/neo4j/users.js
+++ users/models/neo4j/users.js
@@ -1,32 +1,57 @@
 'use strict';
 var fs = require('fs'),
     path = require('path'),
-    driver = require('../helpers/neo4j');
+    q = require('q'),
+    logger = require('../../helpers/logger'),
+    driver = require('../../helpers/neo4j');
 
 var countMutualFriends = function(data) {
     var deferred = q.defer();
     var session = driver.session();
     session
     .run('MATCH (a:Users {userId: {aId}}) \
+         WITH a LIMIT 1 \
          MATCH (b:Users {userId: {bId}}) \
+         WITH b LIMIT 1 \
          MATCH (a)-[:FOLLOW]->(x) \
-         MATCH (b)-[:FOLLOW]->(x) RETURN COUNT(x)', {aId: data.aId, bId: data.bId})
-    .subscribe({
-        onCompleted: function(result) {
-            logger.debug('Count Mutual Friend', data.aId, data.bId);
-            session.close();
-            deferred.resolve(result);
-        },
-        onError: function(error) {
-            logger.error('Failed', error);
-            deferred.reject(error);
-        }
+         MATCH (b)-[:FOLLOW]->(x) RETURN COUNT(x) AS c', {aId: data.aId, bId: data.bId})
+    .then(function(result) {
+        logger.debug('Count Mutual Friend', data.aId, data.bId);
+        session.close();
+        deferred.resolve(result);
+    }).catch(function(error) {
+        logger.error('Failed', error);
+        deferred.reject(error);
     });
     return deferred.promise;
 };
 
+var peopleYouMayKnow = function(data) {
+    var deferred = q.defer();
+    var session = driver.session();
+    session
+    .run('MATCH (u:Users)-[:FOLLOW]->(f:Users)-[:FOLLOW]->(p:Users) \
+        WHERE u.userId = {userId} RETURN DISTINCT p \
+        SKIP {skip} \
+        LIMIT {limit} \
+        ', {userId: data.userId, limit: data.limit, skip: data.skip})
+    .then(function(result) {
+        var userIds = [];
+        result.records.forEach(function(record) {
+            userIds.push(record._fields[0].properties.userId);
+        });
+        session.close();
+        deferred.resolve(userIds);
+    }).catch(function(e) {
+        logger.error('Failed', e);
+        deferred.reject(e);
+    });
+    return deferred.promise;
+}
+
 
 module.exports = {
     modelName: 'User',
-    countMutualFriends: countMutualFriends
+    countMutualFriends: countMutualFriends,
+    peopleYouMayKnow: peopleYouMayKnow
 }
