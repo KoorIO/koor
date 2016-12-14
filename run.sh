14df4747c4e3a31b2d2467deddb3405a130e8cbe
thanhson1085 <thanhson1085@gmail.com>
2016-12-14 15:56:20 +0700

    add Koor.IO logo :tada:

--- run.sh
+++ run.sh
@@ -10,6 +10,15 @@ trap _interupt INT TERM
 
 workdir=${PWD} 
 
+echo '
+ _  _             _  _  
+| |// _  _  _    | |/ \ 
+|  ( /o\/o\/_|   | ( o )
+|_|\\\_/\_/L| () |_|\_/ 
+                        
+'
+                        
+
 if !(docker run -d -p 9200:9200 -p 9300:9300 --name koor_elasticsearch elasticsearch:2.4 > /dev/null 2>&1)
 then
     docker start koor_elasticsearch
