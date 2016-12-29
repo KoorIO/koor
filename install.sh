8ef1a1f612fcb957fd707c6ba3ff9199dfa5f413
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-12-29 16:28:21 +0000

    optimize instal script with github keys inputs :dragon:

--- install.sh
+++ install.sh
@@ -16,6 +16,12 @@ CHATS_PORT=3006
 SOCIALS_PORT=3004
 HOSTNAME=`hostname`
 
+echo "GitHub Client ID? (press enter if no change)"
+read github_client_id
+echo "GitHub Client Secret? (press enter if no change)"
+read github_client_secret
+
+
 echo "Installing Users ..."
 cd $workdir/users && npm install && \
     sed "s/\"host\": \"redis\"/\"host\": \"localhost\"/g" config/default.json > config/local.json && \
@@ -29,6 +35,13 @@ cd $workdir/users && npm install && \
     sed -i "s/es:9200/localhost:9200/g" config/local.json && \
     sed -i "s/bolt:\/\/neo4j/bolt:\/\/localhost/g" config/local.json && \
     sed -i "s/\"port\": 80/\"port\": ${USERS_PORT}/g" config/local.json
+
+if [ "$github_client_secret" != "" ];
+then
+    cd $workdir/users && \
+        sed -i "s/:client_secret:/${github_client_secret}/g" config/local.json
+fi
+
 wait
 echo "Installing Apps ..."
 cd $workdir/apps && npm install && \
@@ -81,6 +94,13 @@ cd $workdir/site && npm install && bower install && \
     sed -i "s/\"mqtt\": \"koor.io\"/\"mqtt\": \"localhost:${APPS_PORT}\"/g" config/local.json && \
     sed -i "s/\"websocket\": \"koor.io\"/\"websocket\": \"localhost:5000\"/g" config/local.json && \
     sed -i "s/\"localEnv\": false/\"localEnv\": true/g" config/local.json
+
+if [ "$github_client_id" != "" ];
+then
+    cd $workdir/site && \
+        sed -i "s/:clientId:/${github_client_id}/g" config/local.json
+fi
+
 wait
 echo "Installing Websocket ..."
 cd $workdir/websocket && npm install && \
