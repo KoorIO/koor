d599f92cad666e3af4c3898e4c03e504a8c25843
Thanh Son <thanhson1085@gmail.com>
2018-09-01 10:14:32 +0000

    remove chat module

--- chats/apis/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var express = require('express'),
-  router = express.Router();
-
-router.use('/api/v1/chats', require('./chats'));
-router.use('/api/v1/groupChats', require('./groupChats'));
-module.exports = router;
