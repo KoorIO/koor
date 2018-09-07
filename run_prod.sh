428da32cbbec3ab9be8a37c1c1166180ea735688
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-09-07 10:16:58 +0700

    Delete run_prod.sh

--- run_prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source .env
-git fetch origin && git checkout origin/master && \
-  docker-compose build && \
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop && \
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml rm -f cms && \
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d && \
-  bash ./clean.sh
